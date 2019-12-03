86937ba039864933cdad67ba371399733eb91752
ToMark1881 <vlad182ava@gmail.com>
2019-12-03 21:10:50 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+package laba_10;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        new EvenCheck().execute("Metallica");
+//
+//        new LunaCheck().execute("19841984");
+//        new LunaCheck().execute("20192019");
+//        new LunaCheck().execute("199919991999");
+
+//        new EAH13().execute("19841984");
+//        new EAH13().execute("20192019");
+//        new EAH13().execute("199919991999");
+//        new InnCheck().execute("123456789012");
+//        new InnCheck().execute("2019201910");
+//        new InnCheck().execute("0987878208");
+//        new TrainCheck().execute("198485");
+//        new TrainCheck().execute("201919");
+//        new TrainCheck().execute("123456");
+//        new ControlSum().execute(26);
+//        new ControlSum().execute(99);
+//        new ControlSum().execute(10);
+        new ErrorsHandler().execute("10111111100");
+
+
+        }
+
+        }
